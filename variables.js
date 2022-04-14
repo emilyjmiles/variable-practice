1ef057c17f7917fb0ecde750a473d1c9b72cfc35
Emily Miles <ms.emily.j.miles@gmail.com>
2022-04-13 22:27:31 -0400

    Fix spelling

--- variables.js
+++ variables.js
@@ -1,9 +1,11 @@
 var cats = "domestic medium hair";
-var dogs = "pitbul";
+var dogs = "pitbull";
 var horses = "pinto";
+
 var weekdays = 5;
 var weekend = 2;
 var age = 34;
+
 var tired = true;
 var late = true;
 var boring = false;
